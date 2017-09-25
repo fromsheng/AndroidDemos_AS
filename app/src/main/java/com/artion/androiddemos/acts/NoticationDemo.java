5f3a9a756ac52924046269643a2d42c2f3b862c6
蔡锦升 <312105857@qq.com>
2017-09-25 09:58:53 +0800

    补充弹通知栏相关代码

--- app/src/main/java/com/artion/androiddemos/acts/NoticationDemo.java
+++ app/src/main/java/com/artion/androiddemos/acts/NoticationDemo.java
@@ -1,7 +1,11 @@
 package com.artion.androiddemos.acts;
 
+import com.artion.androiddemos.R;
 import com.artion.androiddemos.common.NotificationUtils;
 
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.view.View;
 import android.view.View.OnClickListener;
 
@@ -23,6 +27,29 @@ public class NoticationDemo extends CommonBtnDemo {
 				NotificationUtils.showNotification(mAct, "呵呵", btn1.getText().toString());
 				btn1.setText("通知:" + count++);
 			}
+		});
+
+		btn2.setText("测试通知");
+		btn2.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+//				Intent tIntent = mAct.getPackageManager().getLaunchIntentForPackage(getPackageName());
+//				Intent launchIntent = new Intent(Intent.ACTION_MAIN);
+//				launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//				launchIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+//				launchIntent.setComponent(tIntent.getComponent());
+
+				new Thread(new Runnable() {
+					@Override
+					public void run() {
+						Intent launchIntent = new Intent(mAct, ExtraViewDemo.class);
+
+						PendingIntent pendingIntent = PendingIntent.getActivity(mAct, 0, launchIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+						NotificationUtils.showNotification(mAct, getApplicationInfo().icon, "title", "哈哈哈", pendingIntent, 0);
+
+					}
+				}).start();
+				}
 		});
 		
 	}
--- app/src/main/java/com/artion/androiddemos/common/NotificationUtils.java
+++ app/src/main/java/com/artion/androiddemos/common/NotificationUtils.java
@@ -1,15 +1,22 @@
 package com.artion.androiddemos.common;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 
 import com.artion.androiddemos.R;
 import com.artion.androiddemos.acts.AnimationDemo;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Notification相关常用操作类
@@ -41,6 +48,43 @@ public class NotificationUtils {
 		//获取通知管理器对象
 		NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 		mNotificationManager.notify(0, mBuilder.build());
+	}
+
+	public static void showNotification(Context context, int smallIcon, String title, String content, PendingIntent pendingIntent, int notifyId) {
+		if(context == null) {
+			return;
+		}
+
+		NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+		builder.setSmallIcon(smallIcon)
+				.setLargeIcon(BitmapFactory.decodeResource(context.getResources(),
+						context.getApplicationInfo().icon))
+				.setTicker(content)
+				.setContentTitle(title)
+				.setContentText(content)
+				.setStyle(new NotificationCompat.BigTextStyle().bigText(content))//设置为多文本显示
+				.setContentIntent(pendingIntent);
+
+		//横幅通知
+		//因为最新的支持包,只支持到Android 4.1包含此API,低版本只是将浮动通知显示为普通通知,因此这里对运行的版本进行过滤
+//		if (Build.VERSION.SDK_INT > 16) {
+//			//这里预先设置了震动参数,只是为了实现浮动通知效果,真正的震动设置在需要的时候,正式设置
+//			builder.setPriority(Notification.PRIORITY_HIGH).setVibrate(new long[0]);
+//		}
+
+		Notification notification = builder.build();
+
+		//声音
+		notification.defaults |= Notification.DEFAULT_SOUND;
+
+		//震动
+		notification.defaults |= Notification.DEFAULT_VIBRATE;
+
+		//可删除
+		notification.flags |= Notification.FLAG_AUTO_CANCEL;
+
+		NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		mNotificationManager.notify(notifyId, notification);
 	}
 
 }
